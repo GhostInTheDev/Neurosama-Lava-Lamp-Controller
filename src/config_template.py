d795081223067f94e846839e75726b0196564021
Ghost in the Dev <ghost@ghostinthedev.com>
2025-11-06 13:15:28 -0600

    fixed config

---
+++ src/config_template.py
@@ -0,0 +1,11 @@
+# Tuya Device ID (Your Device's unique ID)
+DEVICE_ID = "device_id_here"
+
+# Tuya Local Key (Keep this secret!)
+LOCAL_KEY = "local_key_here"
+
+# Local IP Address of the Tuya Device
+DEVICE_IP = "192.168.x.x"
+
+# Tuya Protocol Version (usually 3.3 or 3.5)
+DEVICE_VERSION = 3.5
